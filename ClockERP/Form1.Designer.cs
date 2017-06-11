ee7648f1d575d46ce5b8357e621dcb7383b5e5fd
Doctor Rubby <563302070@qq.com>
2017-06-11 10:48:21 +0800

    80%

--- ClockERP/Form1.Designer.cs
+++ ClockERP/Form1.Designer.cs
@@ -32,9 +32,12 @@
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button_end = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // webBrowser1
@@ -54,37 +57,68 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(790, 52);
+            this.textBox1.Location = new System.Drawing.Point(886, 27);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(100, 21);
             this.textBox1.TabIndex = 1;
             // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(790, 104);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(100, 21);
-            this.textBox2.TabIndex = 2;
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(820, 160);
+            this.button1.Location = new System.Drawing.Point(823, 91);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
+            this.button1.Text = "开始";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(886, 54);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(100, 21);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // button_end
+            // 
+            this.button_end.Location = new System.Drawing.Point(911, 91);
+            this.button_end.Name = "button_end";
+            this.button_end.Size = new System.Drawing.Size(75, 23);
+            this.button_end.TabIndex = 5;
+            this.button_end.Text = "关闭";
+            this.button_end.UseVisualStyleBackColor = true;
+            this.button_end.Click += new System.EventHandler(this.button_end_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(821, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "相对位置";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(821, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "执行时间";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(917, 469);
+            this.ClientSize = new System.Drawing.Size(1010, 469);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_end);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.webBrowser1);
             this.Name = "Form1";
@@ -100,9 +134,12 @@
         private System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.ToolTip toolTip1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button_end;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- ClockERP/Form1.cs
+++ ClockERP/Form1.cs
@@ -9,6 +9,14 @@ namespace ClockERP
     
     public partial class Form1 : Form, IMessageFilter
     {
+        [DllImport("user32.dll", EntryPoint = "mouse_event", SetLastError = true)]
+        private static extern int mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        private Point p;     
+        private int hour ;
+        private int min ;
+        private int second ;
+
         public Form1()
         {
             InitializeComponent();
@@ -18,6 +26,7 @@ namespace ClockERP
         {
 
             Application.AddMessageFilter(this);
+            dateTimePicker1.Value = DateTime.Now;
         }
 
         public bool PreFilterMessage(ref Message m)
@@ -26,68 +35,63 @@ namespace ClockERP
             {
                 //textBox1.Text =this.PointToClient(Control.MousePosition).X.ToString();
                 //textBox2.Text =this.PointToClient(Control.MousePosition).Y.ToString();
-                Point p = new Point(Control.MousePosition.X - this.Location.X, Control.MousePosition.Y - this.Location.Y);
-                textBox1.Text = p.X.ToString();
-                textBox2.Text = p.Y.ToString();
+                p = new Point(Control.MousePosition.X - this.Location.X, Control.MousePosition.Y - this.Location.Y);
+                textBox1.Text = p.ToString();               
+               
             }
             return false;
         }
 
-        private int relateX;
-        private int relateY;
-
-
-        [DllImport("user32.dll", EntryPoint = "mouse_event", SetLastError = true)]
-        private static extern int mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
-
-
-        public void MouseRightClick()
-        {
-            //点击鼠标右键
-            mouse_event(MouseCommand.MOUSEEVENTF_RIGHTDOWN | MouseCommand.MOUSEEVENTF_RIGHTUP, 410, 0, 0, 0);
-        }
-
-        public void MouseLeftClick()
-        {
-            //点击鼠标左键
-            mouse_event(MouseCommand.MOUSEEVENTF_LEFTDOWN | MouseCommand.MOUSEEVENTF_LEFTUP, 410, 0, 0, 0);
-        }
+    
 
         public void MouseMoveAndClick(Point thisFormLocation)
         {
             
-            int x= (relateX + thisFormLocation.X)* 65536 /Screen.PrimaryScreen.Bounds.Width;
-            int y= (relateY + thisFormLocation.Y) * 65536 /Screen.PrimaryScreen.Bounds.Height;
-            //移动and点击鼠标左键
-            //mouse_event(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE, x, y, 0, 0);
-            //mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
-            //mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+            int x= (p.X + thisFormLocation.X)* 65536 /Screen.PrimaryScreen.Bounds.Width;
+            int y= (p.Y + thisFormLocation.Y) * 65536 /Screen.PrimaryScreen.Bounds.Height;         
             mouse_event(MouseCommand.MOUSEEVENTF_MOVE | MouseCommand.MOUSEEVENTF_ABSOLUTE, x, y, 0, 0);
-
             mouse_event(MouseCommand.MOUSEEVENTF_LEFTDOWN | MouseCommand.MOUSEEVENTF_ABSOLUTE, x, y, 0, 0);//点击
             mouse_event(MouseCommand.MOUSEEVENTF_LEFTUP | MouseCommand.MOUSEEVENTF_ABSOLUTE, x, y, 0, 0);//抬起
 
-            //mouse_event(MouseCommand.MOUSEEVENTF_MOVE | MouseCommand.MOUSEEVENTF_ABSOLUTE, x, y, 0, 0);
+           
         }
    
 
     
         private void timer1_Tick(object sender, EventArgs e)
         {
-            MouseMoveAndClick(this.Location);
+
+            if (DateTime.Now.Second == second&& 
+                DateTime.Now.Minute == min&& 
+                DateTime.Now.Hour == hour)
+            {    
+                MouseMoveAndClick(this.Location);
+            }
+            
         
         }
 
-        private void button1_Click(object sender, EventArgs e)
+     
+
+        private void button_start_Click(object sender, EventArgs e)
         {
-          
-            relateX = Convert.ToInt16(textBox1.Text);
-            relateY = Convert.ToInt16(textBox2.Text);
+            if (p.IsEmpty)
+            {
+                MessageBox.Show("请在屏幕上右键选择点击位置");
+                return;
+            }
+            hour = dateTimePicker1.Value.Hour;
+            min = dateTimePicker1.Value.Minute;
+            second = dateTimePicker1.Value.Second;
+
             timer1.Enabled = true;
-            timer1.Interval = 3000;
+            timer1.Interval = 500;
         }
 
-        
+        private void button_end_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
     }
 
    
